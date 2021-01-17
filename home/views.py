8be345011af92a89caf785dff654bb4a3e76d7ec
haxorsprogramming <haxorsuinsu@gmail.com>
2021-01-17 19:00:05 +0700

    update

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     context = {
-        'judul' : 'Halaman Login',
-        'Developer' : 'Riswanda'
+        'judul' : 'Pengenalan pola jenis tulisan kaligrafi menggunakan metode Zernike Moment dan Support Vector Machine',
+        'Developer' : 'Riswanda Ichsan Himawan'
     }
     return render(request, 'home/home_page.html', context);
--- login/views.py
+++ login/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import mysql.connector
+import json
 
 # Create your views here.
 def login_page(request):
     context = {
-        'judul' : 'Halaman Login',
-        'Developer' : 'Riswanda'
+        'judul' : 'Pengenalan pola jenis tulisan kaligrafi menggunakan metode Zernike Moment dan Support Vector Machine',
+        'Developer' : 'Riswanda Ichsan Himawan'
     }
     return render(request, 'login/login_page.html', context);
